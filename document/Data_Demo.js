f25328f2bbd415d25c6d7a864bb1e0c82e34cebd
Pham Tan Long <phamtanlong1992@gmail.com>
2016-03-02 02:23:35 +0700

    document

---
+++ document/Data_Demo.js
@@ -0,0 +1,34 @@
+
+gAllBusData = //tên biến, ko sửa
+[
+	{
+		number:55, //tuyến xe số mấy
+		object:null, //ko sửa
+		color:"yellow", //màu của tuyến 
+		data:[ //danh sách tọa độ các điểm tạo nên tuyến xe bus
+			[
+				13, //X: tọa độ điểm
+				21, //y: tọa độ điểm
+				0 //thời gian tương đối, bắt đầu từ 0h
+			],
+			[
+				51,
+				35,
+				0.016875
+			] //...
+		],
+		stop:[ //danh sách các trạm trên tuyến
+			[
+				13, //x: tọa độ x
+				21 //y: tọa độ y
+			],
+			[
+				101,
+				71
+			]
+	}
+	//,
+	//{ //tuyến tiếp theo
+		//
+	//}
+];
